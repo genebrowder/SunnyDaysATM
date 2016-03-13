f67bdcdbdea67efd754fc9dde2665a602ae105ba
ebqk6 <ebqk6@umsl.edu>
2016-03-12 22:07:29 -0600

    Solved problem of displaying number for account

--- src/main/java/edu/umsl/sunnyDaysATM/domain/UserLoginInfo.java
+++ src/main/java/edu/umsl/sunnyDaysATM/domain/UserLoginInfo.java
@@ -1,25 +1,28 @@
 package edu.umsl.sunnyDaysATM.domain;
 
+import javax.persistence.Entity;
+
 /**
  * Created by genebrowder on 3/10/16.
  */
+@Entity
 public class UserLoginInfo {
-    int accountNumber;
-    int pin;
+    String accountNumber;
+    String pin;
 
-    public int getAccountNumber() {
+    public String getAccountNumber() {
         return accountNumber;
     }
 
-    public void setAccountNumber(int accountNumber) {
+    public void setAccountNumber(String accountNumber) {
         this.accountNumber = accountNumber;
     }
 
-    public int getPin() {
+    public String getPin() {
         return pin;
     }
 
-    public void setPin(int pin) {
+    public void setPin(String pin) {
         this.pin = pin;
     }
 }
