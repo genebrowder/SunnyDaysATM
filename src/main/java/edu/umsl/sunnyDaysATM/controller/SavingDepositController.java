48a0d1c32c45760cf6fb308b3333124a8748ec77
sal794 <sam.luebbers@gmail.com>
2016-05-11 13:09:47 -0500

    Updated savings deposit and withdrawal

--- src/main/java/edu/umsl/sunnyDaysATM/controller/SavingDepositController.java
+++ src/main/java/edu/umsl/sunnyDaysATM/controller/SavingDepositController.java
@@ -1,5 +1,18 @@
 package edu.umsl.sunnyDaysATM.controller;
 
-public class SavingDepositController {
+import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class SavingDepositController {
+	
+	@RequestMapping(value = {"/saving_deposit"}, method= RequestMethod.POST)
+	public String SavingsDepositSubmit(HttpServletRequest request, Model model){
+		
+		return "saving_deposit_complete";
+	}
 }
--- src/main/java/edu/umsl/sunnyDaysATM/controller/SavingWithdrawalController.java
+++ src/main/java/edu/umsl/sunnyDaysATM/controller/SavingWithdrawalController.java
@@ -1,5 +1,19 @@
 package edu.umsl.sunnyDaysATM.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class SavingWithdrawalController {
 
+	@RequestMapping(value = {"/saving_withdrawal"}, method= RequestMethod.POST)
+	public String SavingsDepositSubmit(HttpServletRequest request, Model model){
+		
+		return "saving_withdrawal_complete";
+	}
+	
 }
